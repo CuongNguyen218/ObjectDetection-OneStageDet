60efe8b0ee6782b2aea20a32264b2ce1fc21901f
CuongNguyen218 <43350150+CuongNguyen218@users.noreply.github.com>
2019-11-12 16:49:56 +0700

    add DW layer

--- yolo/vedanet/network/layer/_darknet.py
+++ yolo/vedanet/network/layer/_darknet.py
@@ -14,7 +14,7 @@ import torch.nn.functional as F
 __all__ = ['Conv2dBatchLeaky', 'Conv2dBatch', 'GlobalAvgPool2d', 'PaddedMaxPool2d', 'Reorg', 'SELayer',
             'CReLU', 'Scale', 'ScaleReLU', 'L2Norm', 'Conv2dL2NormLeaky', 'PPReLU', 'Conv2dBatchPPReLU',
             'Conv2dBatchPReLU', 'Conv2dBatchPLU', 'Conv2dBatchELU', 'Conv2dBatchSELU',
-            'Shuffle', 'Conv2dBatchReLU']
+            'Shuffle', 'Conv2dBatchReLU', 'Conv2dDWBatchLeaky']
 
 
 class Conv2dBatchLeaky(nn.Module):
@@ -58,6 +58,46 @@ class Conv2dBatchLeaky(nn.Module):
         x = self.layers(x)
         return x
 
+class Conv2dDWBatchLeaky(nn.Module):
+    """ This convenience layer groups a 2D convolution, a batchnorm and a leaky ReLU.
+    They are executed in a sequential manner.
+
+    Args:
+        in_channels (int): Number of input channels
+        out_channels (int): Number of output channels
+        kernel_size (int or tuple): Size of the kernel of the convolution
+        stride (int or tuple): Stride of the convolution
+        padding (int or tuple): padding of the convolution
+        leaky_slope (number, optional): Controls the angle of the negative slope of the leaky ReLU; Default **0.1**
+    """
+    def __init__(self, in_channels, out_channels, kernel_size, stride, leaky_slope=0.1):
+        super(Conv2dDWBatchLeaky, self).__init__()
+
+        # Parameters
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        if isinstance(kernel_size, (list, tuple)):
+            self.padding = [int(ii/2) for ii in kernel_size]
+        else:
+            self.padding = int(kernel_size/2)
+        self.leaky_slope = leaky_slope
+
+        # Layer
+        self.layers = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.out_channels, self.kernel_size, self.stride, self.padding,groups=self.in_channels, bias=False),
+            nn.BatchNorm2d(self.out_channels), #, eps=1e-6, momentum=0.01),
+            nn.LeakyReLU(self.leaky_slope, inplace=True)
+        )
+
+    def __repr__(self):
+        s = '{name} ({in_channels}, {out_channels}, kernel_size={kernel_size}, stride={stride}, padding={padding}, negative_slope={leaky_slope})'
+        return s.format(name=self.__class__.__name__, **self.__dict__)
+
+    def forward(self, x):
+        x = self.layers(x)
+        return x
 
 class Conv2dBatchPPReLU(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride):
